11ef1d9cb7f938e7205cd280eab6e9032bb1402e
Davide Rossi <rossidavide108@gmail.com>
2024-02-25 12:05:42 +0100

    🚧 update admin layout

--- src/components/layouts/AdminLayout.tsx
+++ src/components/layouts/AdminLayout.tsx
@@ -1,12 +1,15 @@
+import AdminHeader from '@/components/header/AdminHeader';
+import AdminSidebar from '../sidebar/AdminSidebar';
+
 const DashboardLayout = ({ children } : any) => {
     return (
-        <div className="flex flex-col min-h-screen mx-auto max-w-2xl px-4 pt-8 pb-16">
-        <div className="flex-grow">
-        <h1>Admin Header</h1>
-            <main className="my-0 py-16">{children}</main>
-        </div>
-        <h1>Admin Footer</h1>
-    </div>
+        <>
+            <AdminHeader />
+            <div className="flex h-screen overflow-hidden">
+                <AdminSidebar />
+                <main className="w-full pt-16">{children}</main>
+            </div>
+        </>
     );
   };
   
