80f820b3695bb28d41d45e9c28a9f30c9a180e9f
Davide Rossi <davide.rossi@dieffe.tech>
2024-03-01 10:48:09 +0100

    🔨 update text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import RootLayout from "@/components/layouts/RootLayout";
 
-Home.getLayout = (page : any) => {
+Home.getLayout = (page: any) => {
   return <RootLayout>{page}</RootLayout>;
 };
 
 export default function Home() {
   return (
     <div>
-      <main><h1>Home page</h1></main>
+      <main><h1>Home page modificata</h1></main>
     </div>
   );
 }
